b0de965bdbfd77cf5e7be2016eb332ec3efe0c25
Wael-Amara <waelachrafa2789@nycstudents.net>
2026-09-23 10:17:25 -0400

    g

--- unit-2.py
+++ unit-2.py
@@ -1,2 +1,17 @@
-bill= float()
-print(f"whats your bill {bill}")
+""" bill = float(input("what's your bill "))
+tip = int(input ("whats your tip "))
+print(f"your total is {bill+tip}") """
+
+""" sentence = input("gimme a sentence ")
+word_count = len(sentence.split())
+print ("there are ", word_count, "words") """
+
+
+
+temp = input("whats da temp")
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
