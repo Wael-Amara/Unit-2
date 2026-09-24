1ac469bcd7d95e3714c0782e995035769b7db1de
Wael-Amara <waelachrafa2789@nycstudents.net>
2026-09-24 10:13:41 -0400

    f

--- unit-2.py
+++ unit-2.py
@@ -8,10 +8,28 @@ print ("there are ", word_count, "words") """
 
 
 
-temp = input("whats da temp")
+""" temp = float(input("whats da temp "))
 if temp > 68:
     print('warm')
 elif temp == 68:
     print('perfect')
 else:
     print('cold')
+ """
+
+
+
+""" num = float(input("put in a num "))
+if num % 2 == 1:
+    print ('ts odd just like u')
+elif num % 2 == 0:
+    print ('ts even like the number of jobs u lost')
+else:
+    print ("wth how did u get this message") """
+""" 
+bill = float(input("how much did u get fatty "))
+tip = input("how much u like da food so was it slappin, chill, mid, or horrindus")
+if tip ==  """
+
+x = "test"
+print(f"hello {x}")
